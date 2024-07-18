4b6642a7950c5a333e046fdb78dc598ff23eebf2
tarun261003 <tarunmangalampalli@gmail.com>
2024-07-18 13:57:09 +0530

    Initial Commit

---
+++ GenericPrac/src/main/java/com/gen/Generic.java
@@ -0,0 +1,27 @@
+package com.gen;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.GenericServlet;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.annotation.WebServlet;
+
+/**
+ * Servlet implementation class Generic
+ */
+@WebServlet("/Generic")
+public class Generic extends GenericServlet {
+	private static final long serialVersionUID = 1L;
+
+	/**
+	 * @see Servlet#service(ServletRequest request, ServletResponse response)
+	 */
+	public void service(ServletRequest request, ServletResponse response) throws ServletException, IOException {
+		PrintWriter out = response.getWriter();
+		out.print("<h1>Generic Servlet Checking.</h1>");
+	}
+
+}
