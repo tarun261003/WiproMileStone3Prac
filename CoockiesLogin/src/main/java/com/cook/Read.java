2dfab02ad9fd26f5fd2cd5cf62c0f813a916d41a
tarun261003 <tarunmangalampalli@gmail.com>
2024-07-22 14:44:52 +0530

    Cookies and Login Page

---
+++ CoockiesLogin/src/main/java/com/cook/Read.java
@@ -0,0 +1,49 @@
+package com.cook;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class Read
+ */
+@WebServlet("/Read")
+public class Read extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		response.getWriter().append("Served at: ").append(request.getContextPath());
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		String uname,email,passw;
+		uname=request.getParameter("uname");
+		email=request.getParameter("email");
+		passw=request.getParameter("passw");
+		Cookie c1 = new Cookie("uname", uname);
+		Cookie c2 = new Cookie("email", email);
+		response.addCookie(c1);
+		response.addCookie(c2);
+		response.sendRedirect("./Welcome");
+	}
+
+	/**
+	 * @see HttpServlet#doTrace(HttpServletRequest, HttpServletResponse)
+	 */
+	protected void doTrace(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+	}
+
+}
---
+++ CoockiesLogin/src/main/java/com/cook/Welcome.java
@@ -0,0 +1,42 @@
+package com.cook;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class Welcome
+ */
+@WebServlet("/Welcome")
+public class Welcome extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		String uname,email;
+		Cookie ck[]=request.getCookies();
+		uname=ck[0].getValue();
+		email=ck[1].getValue();
+		PrintWriter out = response.getWriter();
+		out.print("<h1>Welcome to Our website "+uname+"!"+"</h1>");
+		out.print("<h1>Comfirm the email "+email+"</h1>");
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
